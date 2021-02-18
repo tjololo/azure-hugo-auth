6fa7ccfbe57b61e991855fc0c5d53338466e9aab
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-02-18 15:48:49 +0100

    test terraform workflow

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.47.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "hugotf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
