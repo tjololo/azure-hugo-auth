1b7ee5999dd89b3fcca0fd570a2099b9aad83eb4
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-02-19 12:09:31 +0100

    test settingup sas

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,28 @@ resource "azurerm_storage_account" "main" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "content" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+data "azurerm_storage_account_blob_container_sas" "content" {
+  connection_string = azurerm_storage_account.main.primary_connection_string
+  container_name    = azurerm_storage_container.content.name
+  https_only        = true
+
+  start  = "2021-02-18"
+  expiry = "2021-02-20"
+
+  permissions {
+    read   = true
+    add    = false
+    create = false
+    write  = false
+    delete = false
+    list   = false
+  }
+}
 resource "azurerm_app_service_plan" "proxy" {
   name                = "${var.proxyname}-service-plan"
   location            = var.location
@@ -48,4 +70,9 @@ resource "azurerm_function_app" "proxy" {
   storage_account_access_key = azurerm_storage_account.main.primary_access_key
   os_type                    = "linux"
   https_only                 = true
+  app_settings = {
+    "BLOB_SERVICE_ENDPOINT" = azurerm_storage_account.main.primary_blob_endpoint
+    "BLOB_CONTAINER" = azurerm_storage_container.content
+    "BLOB_ACCESS_STRING" = "?${data.azurerm_storage_account_blob_container_sas.content.sas}"
+  }
 }
