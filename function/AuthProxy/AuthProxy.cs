7925ef7b10ca2b0030e8def57ae3ec2ad896555b
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-02-19 21:54:49 +0100

    add allowed users list

--- function/AuthProxy/AuthProxy.cs
+++ function/AuthProxy/AuthProxy.cs
@@ -8,6 +8,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 
 namespace AuthProxy
@@ -16,9 +17,19 @@ namespace AuthProxy
     {
         [FunctionName("AuthProxy")]
         public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get",Route = "{*path}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{*path}")] HttpRequest req,
             ILogger log, string path)
         {
+            // Authorization - allow only @myorg.com users
+            string user = "";
+            if (req.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-NAME")) {
+                user = req.Headers["X-MS-CLIENT-PRINCIPAL-NAME"];
+            }
+            string[] allowesUsers = System.Environment.GetEnvironmentVariable("ALLOWED_USERS").Trim().Split(",");
+            if (allowesUsers.Length > 0 && Array.IndexOf(allowesUsers, user) == -1)
+            {
+                return new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
             var blobStorageURI = System.Environment.GetEnvironmentVariable("BLOB_SERVICE_ENDPOINT", EnvironmentVariableTarget.Process);
             var container = System.Environment.GetEnvironmentVariable("BLOB_CONTAINER", EnvironmentVariableTarget.Process);
             var authKey = System.Environment.GetEnvironmentVariable("BLOB_ACCESS_STRING", EnvironmentVariableTarget.Process);
@@ -26,7 +37,8 @@ namespace AuthProxy
             {
                 client.BaseAddress = new Uri(blobStorageURI);
                 string postfix = "";
-                if (path == null || path.EndsWith("/") || path == ""){
+                if (path == null || path.EndsWith("/") || path == "")
+                {
                     postfix = "index.html";
                 }
                 var storagePath = "/" + container + "/" + path + postfix + authKey;
