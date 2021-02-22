f0dfd05f15f81dc5688b7d9049b8c62be14d2acc
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-02-22 14:37:32 +0100

    cleanup using in auth function

--- function/AuthProxy/AuthProxy.cs
+++ function/AuthProxy/AuthProxy.cs
@@ -1,15 +1,11 @@
-using System.IO;
 using System.Net.Http;
 using System;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Primitives;
-using Newtonsoft.Json;
 
 namespace AuthProxy
 {
